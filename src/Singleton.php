901ae7f941bbf33cb7e7c4815f3ab49504566ecb
Зорин Алексей <zorinalexey59292@gmail.com>
2023-09-11 21:44:51 +0400

    add Singleton pattern

---
+++ src/Singleton.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CloudCastle\AbstractClasses;
+
+abstract class Singleton
+{
+    private static array $instance = [];
+
+    public static function getInstance(): static
+    {
+        return self::$instance[static::class] ?? (self::$instance[static::class] = new static());
+    }
+
+    final protected function __construct()
+    {
+    }
+    final protected function __clone()
+    {
+    }
+}
